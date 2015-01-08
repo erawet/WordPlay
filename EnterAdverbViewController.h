441f7af3a15c3f7aa106947685bea68fad4f8656
Don Wettasinghe <donwettasinghe@Dons-MacBook-Pro.local>
2015-01-07 22:39:27 -0500

    complet all without Autolayout

---
+++ EnterAdverbViewController.h
@@ -0,0 +1,13 @@
+//
+//  EnterAdverbViewController.h
+//  WordPlay
+//
+//  Created by Don Wettasinghe on 1/7/15.
+//  Copyright (c) 2015 Don Wettasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EnterAdverbViewController : UIViewController
+
+@end
---
+++ WordSelectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  WordSelectionViewController.h
+//  WordPlay
+//
+//  Created by Don Wettasinghe on 1/7/15.
+//  Copyright (c) 2015 Don Wettasinghe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WordSelectionViewController : UIViewController
+
+@end
